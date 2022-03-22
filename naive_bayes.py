9d8ec106c4f7b7e5e7937b2e6193d32404c80d04
Olivverde <dele19270@uvg.edu.gt>
2022-03-22 12:41:37 -0600

    Cross Validation completed

--- naive_bayes.py
+++ naive_bayes.py
@@ -241,7 +241,7 @@ class main(object):
         df = self.groupBy_ResponseVar()
         y = df.pop('SaleRange')
         X = df[['LotArea','OverallQual', 'TotRmsAbvGrd', 'GarageCars', 'FullBath']]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3 , random_state=0)
         kf = KFold(n_splits=5)
         clf = LogisticRegression(solver='lbfgs', max_iter=1000)
         clf.fit(X_train, y_train)
